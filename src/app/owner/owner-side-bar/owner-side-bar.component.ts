93e3658d58bf4d568364c4b1504dea6656d76eb9
Sheetal <sheetalpatidar8888@gmail.com>
2024-02-05 11:01:53 +0530

    owner-portal

---
+++ src/app/owner/owner-side-bar/owner-side-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-side-bar',
+  templateUrl: './owner-side-bar.component.html',
+  styleUrls: ['./owner-side-bar.component.css']
+})
+export class OwnerSideBarComponent {
+
+}
